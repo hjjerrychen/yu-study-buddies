6133ee2283b35ca2569b4efc575c924a413cce3e
Jerry Chen <chenhongjin2005@hotmail.com>
2021-05-07 23:45:20 -0400

    put github repo link in header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -9,8 +9,7 @@ function Header(props) {
             <nav className={"navbar border-bottom fixed-top font-weight-bold pl-0 pr-0 " + (props.white ? "navbar-dark " : "navbar-light bg-white")}>
                 <div className="container">
                     <a className="navbar-brand " href="/">Study Buddies @ YU</a>
-                    {/* {props.white && <a className="navbar-right text-white text-decoration-none" href="https://github.com/jerry70450/yu-study-buddies">GitHub</a>} */}
-
+                    {props.white && <a className="navbar-right text-white text-decoration-none" href="https://github.com/jerry70450/yu-study-buddies">GitHub</a>}
                 </div>
             </nav>
         </header>
